fb73fd210b945142498ffb273b8b1f5a10cdb971
Ebentua Philippus Limbong <limmxx.18@gmail.com>
2025-02-22 23:39:39 +0700

    up file loopoing

---
+++ looping.js
@@ -0,0 +1,10 @@
+// For Loop -------------------------------------------------------
+for (let i = 0 ; i < 3 ; i++){
+    console.log(i)
+}
+
+// While Loop -------------------------------------------------------
+let sandi = ""; 
+while(sandi !== "1234"){
+    sandi = prompt("Masukkan sandi : ");
+}
